1868bdaf94b1091421de51bab91c3a9415ae5e7f
Bastien Orivel <eijebong@bananium.fr>
2024-07-25 01:11:17 +0200

    Full CI

--- taskcluster/docker/publish/setup-ssh.sh
+++ taskcluster/docker/publish/setup-ssh.sh
@@ -6,3 +6,7 @@ curl ${DEPLOY_KEY_URL} | jq -r '.secret.ssh_privkey' > ${HOME}/.ssh/id_25519
 
 chmod 700 ${HOME}/.ssh
 chmod 600 ${HOME}/.ssh/id_25519
+
+git config --global user.name "Taskcluster"
+git config --global user.email "eijebong+taskcluster@bananium.fr"
+
