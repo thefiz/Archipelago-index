16a60db69fb3d6fa8e95dee2cb51809d90681277
Bastien Orivel <eijebong@bananium.fr>
2024-07-25 00:16:28 +0200

    Alright, CI time

---
+++ taskcluster/docker/publish/setup-ssh.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+DEPLOY_KEY_URL="http://taskcluster/secrets/v1/secret/Archipelago-index"
+mkdir -p ${HOME}/.ssh
+curl ${DEPLOY_KEY_URL} | jq -r '.secret.ssh_privkey' > ${HOME}/.ssh/id_25519
+
+chmod 700 ${HOME}/.ssh
+chmod 600 ${HOME}/.ssh/id_25519
