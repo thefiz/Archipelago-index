201647c886d8389345cfb55ba71f272b1e8147ef
Bastien Orivel <eijebong@bananium.fr>
2024-07-25 01:14:33 +0200

    Add an older poke crystal to test

--- taskcluster/docker/publish/setup-ssh.sh
+++ taskcluster/docker/publish/setup-ssh.sh
@@ -2,10 +2,12 @@
 
 DEPLOY_KEY_URL="http://taskcluster/secrets/v1/secret/Archipelago-index"
 mkdir -p ${HOME}/.ssh
-curl ${DEPLOY_KEY_URL} | jq -r '.secret.ssh_privkey' > ${HOME}/.ssh/id_25519
+curl ${DEPLOY_KEY_URL} | jq -r '.secret.ssh_privkey' > ${HOME}/.ssh/id_ed25519
+ssh-keyscan github.com > ${HOME}/.ssh/known_hosts
 
 chmod 700 ${HOME}/.ssh
-chmod 600 ${HOME}/.ssh/id_25519
+chmod 600 ${HOME}/.ssh/id_ed25519
+chmod 600 ${HOME}/.ssh/known_hosts
 
 git config --global user.name "Taskcluster"
 git config --global user.email "eijebong+taskcluster@bananium.fr"
