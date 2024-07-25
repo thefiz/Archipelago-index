ba64d32f44effcb756142b9195d95290ed9b2ae5
Bastien Orivel <eijebong@bananium.fr>
2024-07-25 14:50:21 +0200

    Add prod webhook

--- taskcluster/docker/publish/send_webhook.sh
+++ taskcluster/docker/publish/send_webhook.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 ADMIN_KEY_URL="http://taskcluster/secrets/v1/secret/ap-lobby"
-mkdir -p ${HOME}/.ssh
-KEY=$(curl ${ADMIN_KEY_URL} | jq -r '.secret.admin_key_staging')
 
+KEY=$(curl ${ADMIN_KEY_URL} | jq -r '.secret.admin_key_staging')
 curl -H "X-Api-Key: ${KEY}" https://ap-lobby-stg.bananium.fr/worlds/refresh
+
+KEY=$(curl ${ADMIN_KEY_URL} | jq -r '.secret.admin_key_prod')
+curl -H "X-Api-Key: ${KEY}" https://ap-lobby.bananium.fr/worlds/refresh
