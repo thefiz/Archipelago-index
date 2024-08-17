7c8c432b0218035817550f636c1d9d94efc76fcc
Bastien Orivel <eijebong@bananium.fr>
2024-08-17 20:40:11 +0200

    Change how we restart the yaml checkers

--- taskcluster/docker/publish/send_webhook.sh
+++ taskcluster/docker/publish/send_webhook.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-ADMIN_KEY_URL="http://taskcluster/secrets/v1/secret/ap-lobby"
+SECRET_URL="http://taskcluster/secrets/v1/secret/ap-lobby"
+SECRET=$(curl ${SECRET_URL})
 
-KEY=$(curl ${ADMIN_KEY_URL} | jq -r '.secret.admin_key_staging')
+KUBECFG=$(echo "${SECRET}" | jq -r '.secret.kubecfg')
+mkdir -p ${HOME}/.kube
+echo "${KUBECFG}" > ${HOME}/.kube/config
+
+KEY=$(echo "${SECRET}" | jq -r '.secret.admin_key_staging')
 curl -H "X-Api-Key: ${KEY}" https://ap-lobby-stg.bananium.fr/worlds/refresh
+kubectl rollout -n ap-lobby-stg restart deployment yaml-checker-stg
 
-KEY=$(curl ${ADMIN_KEY_URL} | jq -r '.secret.admin_key_prod')
+KEY=$(echo "${SECRET}" | jq -r '.secret.admin_key_prod')
 curl -H "X-Api-Key: ${KEY}" https://ap-lobby.bananium.fr/worlds/refresh
+kubectl rollout -n ap-lobby-prod restart deployment yaml-checker-prod
+
