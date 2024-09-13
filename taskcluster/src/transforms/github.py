75133abef186860513f5f651715e8951388398c8
Bastien Orivel <eijebong@bananium.fr>
2024-09-13 19:55:11 +0200

    Remove print

--- taskcluster/src/transforms/github.py
+++ taskcluster/src/transforms/github.py
@@ -6,7 +6,6 @@ transforms = TransformSequence()
 @transforms.add
 def set_env(config, tasks):
     for task in tasks:
-        print(task)
         env = task["worker"].setdefault("env", {})
         env["GITHUB_PR"] = str(os.environ.get("ARCHIPELAGO_INDEX_PULL_REQUEST_NUMBER", -1))
         yield task
