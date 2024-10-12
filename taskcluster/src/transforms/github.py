70ab7866edb11811eb8684d6f0785a54587a9e7a
Bastien Orivel <eijebong@bananium.fr>
2024-10-12 12:11:17 +0200

    Split the github transform

--- taskcluster/src/transforms/github.py
+++ taskcluster/src/transforms/github.py
@@ -10,16 +10,4 @@ def github_task(config, tasks):
         pr_number = str(os.environ.get("ARCHIPELAGO_INDEX_PULL_REQUEST_NUMBER", -1))
         env["GITHUB_PR"] = pr_number
 
-        task_for = config.params["tasks_for"]
-        task_label = task['name']
-        index_path = f"ap.v2.archipelago-index.{task_label}.level-1.pr.{pr_number}.latest"
-
-        # Re-use indexed PR tasks with comments
-        if task_for == "github-issue-comment":
-            opt = task.setdefault("optimization", {})
-            skip_unless_changed = opt.pop("skip-unless-changed", [])
-            task["optimization"] = {"skip-unless-changed-or-cached": {"index-path": index_path, "skip-unless-changed": skip_unless_changed}}
-        elif task_for.startswith("github-pull-request"):
-            task.setdefault("routes", []).append(f"index.{index_path}")
-
         yield task
---
+++ taskcluster/src/transforms/github_cached.py
@@ -0,0 +1,23 @@
+from taskgraph.transforms.base import TransformSequence
+import os
+
+transforms = TransformSequence()
+
+@transforms.add
+def github_task(config, tasks):
+    for task in tasks:
+        pr_number = str(os.environ.get("ARCHIPELAGO_INDEX_PULL_REQUEST_NUMBER", -1))
+
+        task_for = config.params["tasks_for"]
+        task_label = task['name']
+        index_path = f"ap.v2.archipelago-index.{task_label}.level-1.pr.{pr_number}.latest"
+
+        # Re-use indexed PR tasks with comments
+        if task_for == "github-issue-comment":
+            opt = task.setdefault("optimization", {})
+            skip_unless_changed = opt.pop("skip-unless-changed", [])
+            task["optimization"] = {"skip-unless-changed-or-cached": {"index-path": index_path, "skip-unless-changed": skip_unless_changed}}
+        elif task_for.startswith("github-pull-request"):
+            task.setdefault("routes", []).append(f"index.{index_path}")
+
+        yield task
